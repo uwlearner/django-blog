1755a0f421f32e7d690f311399631769ebae273f
uwlearner <>
2020-02-15 16:41:27 -0800

    ***begin Lesson7*** add categories to blogging app

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from blogging.models import Post
+from blogging.models import Post, Category
 
 
 admin.site.register(Post)
+admin.site.register(Category)
--- blogging/models.py
+++ blogging/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
+
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -11,3 +12,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
